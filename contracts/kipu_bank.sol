f17093442b2657c63cbf80b4a722b23a3b1cf98b
Bricio <briciocardoso@gmail.com>
2025-09-15 22:45:42 +0000

    add make deposit function

---
+++ contracts/kipu_bank.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.30;
+
+contract KipuBank {
+
+  mapping(address => uint) balances;
+
+  function _makeDeposit(uint _amount) private {
+    balances[msg.sender] += _amount;
+  }
+}
