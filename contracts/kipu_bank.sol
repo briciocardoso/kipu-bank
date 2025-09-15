f99ddd10975af7f6fcb7f7fd0ae1465d9d00f9d5
Bricio <briciocardoso@gmail.com>
2025-09-15 22:47:25 +0000

    add a withdrawal limit

--- contracts/kipu_bank.sol
+++ contracts/kipu_bank.sol
@@ -5,12 +5,20 @@ pragma solidity ^0.8.30;
 contract KipuBank {
 
   mapping(address => uint) balances;
+  uint immutable withdrawLimit;
+
+  constructor(uint _withdrawLimit) {
+    withdrawLimit = _withdrawLimit;
+  }
 
   function _makeDeposit(uint _amount) private {
     balances[msg.sender] += _amount;
   }
 
   function _makeWithdraw(uint _amount) private {
+    if(_amount > withdrawLimit)
+      revert("Withdrawal amount exceeds limit");
+
     balances[msg.sender] -= _amount;
   }
 }
