591170fbd0d17d10d2ca608448c31ef3aa9b534e
Bricio <briciocardoso@gmail.com>
2025-10-07 02:54:59 +0000

    add getTokenUsdValue

--- contracts/bank_factory.sol
+++ contracts/bank_factory.sol
@@ -4,7 +4,9 @@ pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 /// @title KipuFactory
 /// @author Bricio Cardoso
@@ -13,6 +15,7 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 contract BankFactory is AccessControl {
   using SafeERC20 for IERC20;
 
+  address public constant ETH_ADDRESS = address(0);
   /// @notice The bank capacity amount to receive deposits in Wei
   uint immutable public usdBankCapacity;
   /// @notice The withdraw limit in Wei
@@ -44,4 +47,17 @@ contract BankFactory is AccessControl {
     isTokenAllowed[_token] = false;
     emit TokenRemoved(_token);
   }
+
+  function getTokenUsdValue(address _token, uint _amount) public view returns (uint) {
+    address priceFeedAddress = priceFeeds[_token];
+    
+    if (priceFeedAddress == address(0)) revert InvalidPriceFeed(_token);
+
+    AggregatorV3Interface priceFeed = AggregatorV3Interface(priceFeedAddress);
+    (, int256 price, , , ) = priceFeed.latestRoundData();
+    uint256 priceFeedDecimals = priceFeed.decimals();
+    uint256 tokenDecimals = (_token == ETH_ADDRESS) ? 18 : IERC20Metadata(_token).decimals();
+
+    return (_amount * uint(price) * (10**18)) / ((10**tokenDecimals) * (10**priceFeedDecimals));
+    }
 }
